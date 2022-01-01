027aec248b16ed43eb4379b71147c225004cc389
alihaij <a3haij@gmail.com>
2022-01-01 17:20:07 +0300

    My First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                    radius: 50, backgroundImage: AssetImage('images/Ali.jpg')),
+                Text('Ali Haij',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 50.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text('FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(color: Colors.teal.shade100),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+966 568164020',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                          letterSpacing: 0.5,
+                          fontFamily: 'SourceSansPro',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'alihaij@gmail.com',
+                        style: TextStyle(
+                          fontSize: 20,
+                          letterSpacing: 0.5,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )),
+              ],
+            ))));
+  }
 }
